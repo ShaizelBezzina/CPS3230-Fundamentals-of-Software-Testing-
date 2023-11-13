99e2c5c923a37d1488975c4c347b6a5b61d13e2f
Shaizel <103184827+ShaizelBezzina@users.noreply.github.com>
2023-11-13 15:46:16 +0000

    Update WeatherRecommendationTest.java

--- src/test/java/WeatherWear/com/WeatherRecommendationTest.java
+++ src/test/java/WeatherWear/com/WeatherRecommendationTest.java
@@ -1,19 +1,72 @@
 package WeatherWear.com;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 public class WeatherRecommendationTest {
 
     @Test
     void testRecommendClothingForCurrentLocation() {
-        // Calling the method to ensure it doesn't throw an exception
-        WeatherRecommendation.recommendClothingForCurrentLocation();
+        // Mocking some dependencies
+        WeatherService weatherService = mock(WeatherService.class);
+        ClothingRecommendationService clothingRecommendationService = mock(ClothingRecommendationService.class);
+
+        // Creating an instance of WeatherRecommendation with mocked dependencies
+        WeatherRecommendation weatherRecommendation = new WeatherRecommendation(weatherService, clothingRecommendationService);
+
+        // Testing the method
+        weatherRecommendation.recommendClothingForCurrentLocation();
+
+        // Verifying that certain methods were called
+        verify(weatherService, times(1)).getCurrentWeather();
+        verify(clothingRecommendationService, times(1)).recommendClothing(any());
     }
 
     @Test
     void testRecommendClothingForFutureLocation() {
-        // Calling the method to ensure it doesn't throw an exception
-        WeatherRecommendation.recommendClothingForFutureLocation();
+        // Mocking some dependencies
+        WeatherService weatherService = mock(WeatherService.class);
+        ClothingRecommendationService clothingRecommendationService = mock(ClothingRecommendationService.class);
+
+        // Creating an instance of WeatherRecommendation with mocked dependencies
+        WeatherRecommendation weatherRecommendation = new WeatherRecommendation(weatherService, clothingRecommendationService);
+
+        // Testing the method
+        weatherRecommendation.recommendClothingForFutureLocation();
+
+        // Verifying that certain methods were called
+        verify(weatherService, times(1)).getFutureWeather(any());
+        verify(clothingRecommendationService, times(1)).recommendClothing(any());
+    }
+
+    // Dummy interfaces for demonstration purposes
+    interface WeatherService {
+        String getCurrentWeather();
+        String getFutureWeather(String date);
+    }
+
+    interface ClothingRecommendationService {
+        String recommendClothing(String weather);
+    }
+
+    // Example WeatherRecommendation class
+    static class WeatherRecommendation {
+        private final WeatherService weatherService;
+        private final ClothingRecommendationService clothingRecommendationService;
+
+        public WeatherRecommendation(WeatherService weatherService, ClothingRecommendationService clothingRecommendationService) {
+            this.weatherService = weatherService;
+            this.clothingRecommendationService = clothingRecommendationService;
+        }
+
+        public void recommendClothingForCurrentLocation() {
+            String currentWeather = weatherService.getCurrentWeather();
+            clothingRecommendationService.recommendClothing(currentWeather);
+        }
+
+        public void recommendClothingForFutureLocation() {
+            String futureWeather = weatherService.getFutureWeather("some future date");
+            clothingRecommendationService.recommendClothing(futureWeather);
+        }
     }
 }
-
